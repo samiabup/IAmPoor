42e129d6aab4fe01cb374f6d29649182251e1293
Samira Akter <samira@Mds-MacBook-Air-2.local>
2019-01-22 22:04:08 +0600

    First Commit

---
+++ I am poor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by Samira Akter on 1/20/19.
+//  Copyright © 2019 Samira Akter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
